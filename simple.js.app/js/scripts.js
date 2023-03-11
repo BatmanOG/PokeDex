f76bd4b10f6aaacc3e63ed64ddbe6ae599c43920
BatmanOG <122240285+BatmanOG@users.noreply.github.com>
2023-03-11 09:25:49 -0500

    Update scripts.js
    if committed this will add multiple pokemon to the array

--- simple.js.app/js/scripts.js
+++ simple.js.app/js/scripts.js
@@ -1,3 +1,11 @@
-alert ('Hello World');
-let favoriteFood = 'Pizza'
-document.write (favoriteFood)
+let pokemonList = [
+    { name: "Bulbasaur", height: 70, weight: 6.9, types:['Grass','Poison'], eggGroups:['Monster', 'Grass'],catchRate: 0, number: 1 },
+    { name: "Ivysaur", height: 100, weight: 13, types:['Grass','Poison'], eggGroups:['Monster', 'Grass'],catchRate: 0 , number: 2 },
+    { name: "Venusaur", height: 200, weight: 100, types:['Grass','Poison'], eggGroups:['Monster', 'Grass'],catchRate: 0, number: 3 },
+    { name: "Charmander", height: 60, weight: 8.5, types:['Fire'], eggGroups:['Monster','Dragon'], catchRate: 0, number: 4 },
+    { name: "Charmeleon", height: 110, weight: 19, types:['Fire'], eggGroups:['Monster','Dragon'], catchRate: 0, number: 5 },
+    { name: "Charzard", height: 170, weight: 90.5, types:['Fire','Flying'], eggGroups:['Monster','Dragon'], catchRate: 0, number:6 },
+    { name: "Squirtle", height: 50, weight: 9, types:['Water'], eggGroups:['Monster','Water'], catchRate: 0, number: 7 },
+    { name: "Wartortle", height: 100, weight: 22.5,types:['Water'], eggGroups:['Monster','Water'], catchRate: 0, number: 8 },
+    { name: "Blastoise", height: 160, weight: 85.5, types:['Water'], eggGroups:['Monster','Water'], catchRate: 0, number: 9 }
+]
