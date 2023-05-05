679ad6891a0372d663e2958c7ed16bf7a32efb79
BatmanOG <122240285+BatmanOG@users.noreply.github.com>
2023-05-05 10:20:48 -0400

    when commited this will add the styling for the buttons.

--- simple.js.app/js/scripts.js
+++ simple.js.app/js/scripts.js
@@ -78,21 +78,31 @@ let pokemonList = [
         pokemonList.push(pokemon);
     }
 
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listpokemon = document.createElement('li');
+        let button =document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+    }
+
     function getAll() {
         return pokemonList
     }
+
     return {
         add: add,
         getAll: getAll,
+        addListItem: addListItem,
     };
 })();
 
 // adding for and if statements to print out names on my si
-pokemonRepository.getAll().forEach(function(user){
-    if ( user.height >150 ){
-    document.write( "<p>" + user.name + " ( Height " + user.height + " cm ) - Wow, that's a big Pokemon " + "</p>");
-}   else {document.write("<p>" + user.name + " ( Height " + user.height + " cm ) " + "</p>" );
-}
+pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+
 });
     
     
